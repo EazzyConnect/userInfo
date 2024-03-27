dd5d346e47c6593bd7a3f59db2b6b495d0745243
Isaiah Okon <izaya4real@gmail.com>
2024-03-27 19:46:58 +0100

    authentication

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import inputCSS from "./css-files/Input.module.css"
 
-const Input = ({ type, name, id, placeholder, value }) => {
+const Input = ({ type, name, id, placeholder, value, onchange }) => {
 
  return (
   <>
 
    <div id={inputCSS.inputContainer}>
     <label className={inputCSS.label} htmlFor={id}>{id}: </label>
-    <input className={inputCSS.input} type={type} name={name} id={id} placeholder={placeholder} value={value} />
+    <input className={inputCSS.input} type={type} name={name} id={id} placeholder={placeholder} value={value} onChange={onchange} />
    </div>
 
   </>
--- src/components/SigunUp.jsx
+++ src/components/SigunUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './Input';
 import signUpCSS from './css-files/SignUp.module.css';
 
@@ -6,10 +6,52 @@ import signUpCSS from './css-files/SignUp.module.css';
 
 const SignUp = ({ logInTab }) => {
 
+ const [userDetails, setUserDetails] = useState({
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirm_Password: ""
+ })
+
  const signUpBtn = (e) => {
   e.preventDefault();
   console.log(`Signing Up...`);
+  console.log(userDetails);
  };
+
+ const handleChange = (e) => {
+  let name = e.target.name;
+  let value = e.target.value;
+  setUserDetails((prevData) => ({
+   ...prevData,
+   [name]: value
+  }))
+ }
+
+ const authCheck = () => {
+  if (
+   userDetails.firstName === "" ||
+   userDetails.lastName === "" ||
+   userDetails.email === "" ||
+   userDetails.password === "" ||
+   userDetails.confirm_Password === ""
+  ) {
+   alert("Provide all fields")
+  }
+
+  if (userDetails.password !== userDetails.confirm_Password) {
+   alert("Password does not match")
+  }
+
+  if (userDetails.password.length < 6) {
+   alert("Password length is less than 6 characters")
+  } else {
+   alert("Registration Successful")
+   location.reload()
+  }
+
+ }
 
  return (
   <>
@@ -19,12 +61,12 @@ const SignUp = ({ logInTab }) => {
     <h1>Sign-up Page</h1>
 
     <form action="" method='POST' onSubmit={signUpBtn}>
-     <Input type={"text"} id={"FirstName"} placeholder={"Enter First Name"} />
-     <Input type={"text"} id={"LastName"} placeholder={"Enter Last Name"} />
-     <Input type={"email"} id={"Email"} placeholder={"Enter Email"} />
-     <Input type={"password"} id={"Password"} placeholder={"Enter Password"} />
-     <Input type={"password"} id={"Confirm-Password"} placeholder={"Enter Password"} />
-     <button type="submit">SIGN UP</button>
+     <Input type={"text"} id={"FirstName"} placeholder={"Enter First Name"} value={userDetails.firstName} name={"firstName"} onchange={handleChange} />
+     <Input type={"text"} id={"LastName"} placeholder={"Enter Last Name"} value={userDetails.lastName} name={"lastName"} onchange={handleChange} />
+     <Input type={"email"} id={"Email"} placeholder={"Enter Email"} value={userDetails.email} name={"email"} onchange={handleChange} />
+     <Input type={"password"} id={"Password"} placeholder={"Enter Password"} value={userDetails.password} name={"password"} onchange={handleChange} />
+     <Input type={"password"} id={"Confirm-Password"} placeholder={"Enter Password"} value={userDetails.confirm_Password} name={"confirm_Password"} onchange={handleChange} />
+     <button type="submit" onClick={authCheck}>SIGN UP</button>
     </form>
 
     <div id={signUpCSS.bottomDiv}>
